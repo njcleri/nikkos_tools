3704be86591a2b5f31cf299e2e98af444f7ae1e6
Nikko Cleri <nikko.cleri@gmail.com>
2023-09-23 10:15:54 -0500

    renamed wavelength_conversion

--- spectrum_tools/display_lines.py
+++ spectrum_tools/display_lines.py
@@ -10,14 +10,14 @@ def load_lines(file):
         print('FileNotFoundError: lines file not found')
 
 def display_lines_air(ax, vertical_anchor, lines_to_show=[], redshift=0, 
-                      wavelength_conversion=1, width=20, fontsize=15, show_text=False, lines_file=LINESDF_PATH):
+                      wavelength_per_angstrom=1, width=20, fontsize=15, show_text=False, lines_file=LINESDF_PATH):
     linesdf = load_lines(lines_file)
     
     for line in lines_to_show:
         line_data = linesdf[linesdf.line == line]
-        wave_min = (line_data.wavelength_air.values[0]-width/2)*wavelength_conversion*(1+redshift)
-        wave_max = (line_data.wavelength_air.values[0]+width/2)*wavelength_conversion*(1+redshift)
-        offset = line_data.offset.values[0]*wavelength_conversion*(1+redshift)
+        wave_min = (line_data.wavelength_air.values[0]-width/2)*wavelength_per_angstrom*(1+redshift)
+        wave_max = (line_data.wavelength_air.values[0]+width/2)*wavelength_per_angstrom*(1+redshift)
+        offset = line_data.offset.values[0]*wavelength_per_angstrom*(1+redshift)
         label = line_data.label.values[0]
         if type(label) != str:
             label = ''
@@ -32,14 +32,14 @@ def display_lines_air(ax, vertical_anchor, lines_to_show=[], redshift=0,
             fontsize=fontsize, verticalalignment='top', horizontalalignment='right', rotation='vertical')
             
 def display_lines_vacuum(ax, vertical_anchor, lines_to_show=[], redshift=0, 
-                      wavelength_conversion=1, width=20, fontsize=15, show_text=False, lines_file='linesdf.csv'):
+                      wavelength_per_angstrom=1, width=20, fontsize=15, show_text=False, lines_file=LINESDF_PATH):
     linesdf = load_lines(lines_file)
     
     for line in lines_to_show:
         line_data = linesdf[linesdf.line == line]
-        wave_min = (line_data.wavelength_vacuum.values[0]-width/2)*wavelength_conversion*(1+redshift)
-        wave_max = (line_data.wavelength_vacuum.values[0]+width/2)*wavelength_conversion*(1+redshift)
-        offset = line_data.offset.values[0]*wavelength_conversion*(1+redshift)
+        wave_min = (line_data.wavelength_vacuum.values[0]-width/2)*wavelength_per_angstrom*(1+redshift)
+        wave_max = (line_data.wavelength_vacuum.values[0]+width/2)*wavelength_per_angstrom*(1+redshift)
+        offset = line_data.offset.values[0]*wavelength_per_angstrom*(1+redshift)
         label = line_data.label.values[0]
         if type(label) != str:
             label = ''
--- spectrum_tools/make_linesdf.py
+++ spectrum_tools/make_linesdf.py
@@ -2,60 +2,32 @@ import pandas as pd
 from nikkos_tools import physics_functions as pf
 
 lines = {
-        'Lya':[1215.67,r'Ly$\alpha$'],
-        'NV':[1240,'NV'],
-        'CIV_1548':[1548,'CIV'],
-        'HeII_1640':[1640.4,'HeII'],
-        'OIII_1664':[1664,'OIII]'],
-        'SiIII_1883':[1883,'SiIII]'],
-        'CIII_1907':[1906.8,'CIII]'],
-        'MgII_2798':[2798,'MgII'],
-        'NeV_3346':[3346,''],
-        'NeV_3426':[3426,'[NeV]'],
-        'OII_3727':[3727,'[OII]'],
-        'NeIII_3869':[3869,'[NeIII]'],
-        'Hdelta':[4102,r'H$\delta$'],
-        'Hgamma':[4341,r'H$\gamma$'],
-        'OIII_4363':[4363,'[OIII]'],
-        'HeII_4686':[4686,'HeII'],
-        'Hbeta':[4861, r'H$\beta$'],
-        'OIII_4959':[4959,''],
-        'OIII_5007':[5007,'[OIII]'],
-        'HeI':[5876,'HeI'],
-        'Halpha':[6563, r'H$\alpha$'],
-        'SII_6716':[6716,''],
-        'SII_6731':[6731,'[SII]']
-		}
-
-shifts = {
-        'Lya':-1,
-        'NV':1,
-        'CIV_1548':-1,
-        'HeII_1640':1,
-        'OIII_1664':1,
-        'SiIII_1883':-1,
-        'CIII_1907':1,
-        'MgII_2798':1,
-        'NeV_3346':-1,
-        'NeV_3426':-1,
-        'OII_3727':-1,
-        'NeIII_3869':1,
-        'Hdelta':1,
-        'Hgamma':-1,
-        'OIII_4363':1,
-        'HeII_4686':-1,
-        'Hbeta':-1,
-        'OIII_4959':1,
-        'OIII_5007':1,
-        'HeI':-1,
-        'Halpha':-1,
-        'SII_6716':-1,
-        'SII_6731':-1
-		}
+    'Lya': [1215.67, 'Ly$\\alpha$', -1],
+    'NV': [1240, 'NV', 1],
+    'CIV_1548': [1548, 'CIV', -1],
+    'HeII_1640': [1640.4, 'HeII', 1],
+    'OIII_1664': [1664, 'OIII]', 1],
+    'SiIII_1883': [1883, 'SiIII]', -1],
+    'CIII_1907': [1906.8, 'CIII]', 1],
+    'MgII_2798': [2798, 'MgII', 1],
+    'NeV_3346': [3346, '', -1],
+    'NeV_3426': [3426, '[NeV]', -1],
+    'OII_3727': [3727, '[OII]', -1],
+    'NeIII_3869': [3869, '[NeIII]', 1],
+    'Hdelta': [4102, 'H$\\delta$', 1],
+    'Hgamma': [4341, 'H$\\gamma$', -1],
+    'OIII_4363': [4363, '[OIII]', 1],
+    'HeII_4686': [4686, 'HeII', -1],
+    'Hbeta': [4861, 'H$\\beta$', -1],
+    'OIII_4959': [4959, '', 1],
+    'OIII_5007': [5007, '[OIII]', 1],
+    'HeI': [5876, 'HeI', -1],
+    'Halpha': [6563, 'H$\\alpha$', -1],
+    'SII_6716': [6716, '', -1],
+    'SII_6731': [6731, '[SII]', -1]
+ }
 
 def make_linesdf(save_path='./linesdf.csv'):
-    for key in lines.keys():
-        lines.get(key).append(shifts.get(key))
     linesdf = (pd.DataFrame(lines)
             .T
             .reset_index()
