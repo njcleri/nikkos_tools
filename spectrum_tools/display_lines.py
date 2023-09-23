e2a016204a75b06aaeafb9c2e1ac4105267bfa53
Nikko Cleri <nikko.cleri@gmail.com>
2023-09-23 11:41:33 -0500

    refactored display_lines

--- spectrum_tools/display_lines.py
+++ spectrum_tools/display_lines.py
@@ -3,6 +3,22 @@ from pathlib import Path
 BASE_PATH = Path(__file__).parent
 LINESDF_PATH = (BASE_PATH / "linesdf.csv").resolve()
 
+def generate_default_axvspan_kwargs():
+    axvspan_kwargs = {
+        'color':'k',
+        'ec':None,
+        'alpha':0.1,
+    }
+    return axvspan_kwargs
+
+def generate_default_text_kwargs():
+    text_kwargs = {
+        'fontsize':15,
+        'rotation':'vertical',
+        'verticalalignment':'top'
+    }
+    return text_kwargs
+
 def load_lines(file):
     try:
         return pd.read_csv(file)
@@ -10,8 +26,10 @@ def load_lines(file):
         print('FileNotFoundError: lines file not found')
 
 def display_lines_air(ax, vertical_anchor, lines_to_show=[], redshift=0, 
-                      wavelength_per_angstrom=1, width=20, fontsize=15, show_text=False, lines_file=LINESDF_PATH):
+                      wavelength_per_angstrom=1, width=20, show_text=False, lines_file=LINESDF_PATH, axvspan_kwargs={}, text_kwargs={}):
     linesdf = load_lines(lines_file)
+    axvspan_kwargs = generate_default_axvspan_kwargs() | axvspan_kwargs
+    text_kwargs = generate_default_text_kwargs() | text_kwargs
     
     for line in lines_to_show:
         line_data = linesdf[linesdf.line == line]
@@ -22,18 +40,18 @@ def display_lines_air(ax, vertical_anchor, lines_to_show=[], redshift=0,
         if type(label) != str:
             label = ''
 
-        ax.axvspan(wave_min, wave_max, color='k', ec=None, alpha=0.1)
+        ax.axvspan(wave_min, wave_max, **axvspan_kwargs)
         if show_text and (offset > 0):
-            ax.text((wave_max), vertical_anchor, f'{label}',\
-            fontsize=fontsize, verticalalignment='top', horizontalalignment='left', rotation='vertical')
+            ax.text(wave_max, vertical_anchor, f'{label}', horizontalalignment='left', **text_kwargs)
         
         if show_text and (offset < 0):
-            ax.text((wave_max), vertical_anchor, f'{label}',\
-            fontsize=fontsize, verticalalignment='top', horizontalalignment='right', rotation='vertical')
+            ax.text(wave_min, vertical_anchor, f'{label}', horizontalalignment='right', **text_kwargs)
             
-def display_lines_vacuum(ax, vertical_anchor, lines_to_show=[], redshift=0, 
-                      wavelength_per_angstrom=1, width=20, fontsize=15, show_text=False, lines_file=LINESDF_PATH):
+def display_lines_air(ax, vertical_anchor, lines_to_show=[], redshift=0, 
+                      wavelength_per_angstrom=1, width=20, show_text=False, lines_file=LINESDF_PATH, axvspan_kwargs={}, text_kwargs={}):
     linesdf = load_lines(lines_file)
+    axvspan_kwargs = generate_default_axvspan_kwargs() | axvspan_kwargs
+    text_kwargs = generate_default_text_kwargs() | text_kwargs
     
     for line in lines_to_show:
         line_data = linesdf[linesdf.line == line]
@@ -44,11 +62,11 @@ def display_lines_vacuum(ax, vertical_anchor, lines_to_show=[], redshift=0,
         if type(label) != str:
             label = ''
 
-        ax.axvspan(wave_min, wave_max, color='k', ec=None, alpha=0.1)
+        ax.axvspan(wave_min, wave_max, **axvspan_kwargs)
         if show_text and (offset > 0):
-            ax.text((wave_max), vertical_anchor, f'{label}',\
-            fontsize=fontsize, verticalalignment='top', horizontalalignment='left', rotation='vertical')
+            ax.text(wave_max, vertical_anchor, f'{label}', horizontalalignment='left', **text_kwargs)
         
         if show_text and (offset < 0):
-            ax.text((wave_max), vertical_anchor, f'{label}',\
-            fontsize=fontsize, verticalalignment='top', horizontalalignment='right', rotation='vertical')
+            ax.text(wave_min, vertical_anchor, f'{label}', horizontalalignment='right', **text_kwargs)
+            
+            
