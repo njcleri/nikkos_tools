cca0b0e0063436d2d601ca1beb84a98e39d042dd
Nikko Cleri <nikko.cleri@gmail.com>
2024-02-01 09:32:40 -0600

    added email notifications

--- cloudy_tools/cloudy_slurm_builder.py
+++ cloudy_tools/cloudy_slurm_builder.py
@@ -15,10 +15,10 @@ class CloudySlurm:
     def skip_line(self):
         self.set_parameter(f'')
         
-    def set_export(self, export='NONE'):
+    def set_export(self, export):
         self.set_parameter(f'#SBATCH --export={export}')
     
-    def set_user_env(self, user_env='L'):
+    def set_user_env(self, user_env):
         self.set_parameter(f'#SBATCH --get-user-env={user_env}')
         
     def set_job_name(self, job_name):
@@ -40,26 +40,33 @@ class CloudySlurm:
     def set_output(self, output):
         self.set_parameter(f'#SBATCH --output={output}.%j')
         
+    def send_email_notification(self, email, type):
+        self.set_parameter(f'#SBATCH --mail-type={type}')
+        self.set_parameter(f'#SBATCH --mail-user={email}')
+        
     def set_executable_line(self, executable_line):
         self.set_parameter(f'{executable_line}')
     
     def set_cloudy_executable_line(self, cloudy_executable, cloudy_input):
         self.set_parameter(f'{cloudy_executable} {cloudy_input}')
         
-    def build_default_slurm(self, job_name='test_job', time='01:00:00', ntasks=1, ntasks_per_node=1, mem='2560M', output='test_job', cloudy_executable='run_cloudy', cloudy_input='test'):
+    def build_default_slurm(self, job_name='test_job', time='01:00:00', ntasks=1, ntasks_per_node=None, mem='2560M', output='test_job', email=None, notification_type='ALL', cloudy_executable='run_cloudy', cloudy_input='test'):
         self.slurm = []
         self.set_parameter(f'#!/bin/bash')
-        self.set_parameter(f'##ENVIRONMENT SETTINGS; CHANGE WITH CAUTION')
-        self.set_export()
-        self.set_user_env()
-        self.skip_line()
+        # self.set_parameter(f'##ENVIRONMENT SETTINGS; CHANGE WITH CAUTION')
+        # self.set_export()
+        # self.set_user_env()
+        # self.skip_line()
         self.set_parameter(f'##NECESSARY JOB SPECIFICATIONS')
         self.set_job_name(job_name)
         self.set_time(time)
         self.set_ntasks(ntasks)
-        self.set_ntasks_per_node(ntasks_per_node)
+        if ntasks_per_node:
+            self.set_ntasks_per_node(ntasks_per_node)
         self.set_mem(mem)
         self.set_output(output)
+        if email:
+            self.send_email_notification(email, notification_type)
         self.skip_line()
         if type(cloudy_input) != list:
             self.set_cloudy_executable_line(cloudy_executable, cloudy_input)
